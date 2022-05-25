c931ecbaf26fd02bf2ea33e1bca757ea832119a1
siddhantsingh1996 <siddhant.singh@roro.io>
2022-05-25 18:33:17 +0530

    assingmenrt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 function App() {
+  const [update, setUpdate] = useState(0);
+
+ 
+  
+  const startBtn = () => {
+    let myInterval = setInterval(() => {
+      setUpdate((prev)=>prev+10);
+      console.log("update",update);
+    }, 1000)
+    
+    setTimeout(()=>{
+       clearInterval(myInterval);
+    },10000)
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="progress-bar-wrapper">
+        <div className="progress">
+          <div style={{width:(update*4).toString()+'px',backgroundColor:'#61dafb',borderRadius:'12px',height:'20px',color:'#000000',display:'flex',justifyContent:'flex-end'}}>{update===0?"":update}</div>
+        </div>
+        <div
+          className="start-btn"
+          onClick={() => {
+            startBtn();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Start
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//abhishek.a@wayforward.io
